e621fc06e390b0d928e02ebf8263a51ef2c9fd4d
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-24 16:50:47 -0300

    Adiciona teste Elefante Incomoda

---
+++ testeElefanteIncomoda/Program.cs
@@ -0,0 +1,24 @@
+﻿public class ElefanteIncomoda
+{
+   public string criarString(int qtd)
+   {
+      string qtdIncomoda = "";
+
+      for (var i = 0; i < qtd; i++)
+      {
+         qtdIncomoda = qtdIncomoda + "Incomoda ";
+      }
+
+      return qtdIncomoda;
+   }
+   public static void Main(string[] args)
+   {
+      Console.WriteLine("Digite um numero para terminar a musica:");
+      Console.WriteLine("Um elefante incomoda muita gente...");
+      string strQtd = Console.ReadLine();
+      int numQtd = Int16.Parse(strQtd);
+      ElefanteIncomoda metodo = new ElefanteIncomoda();
+      string incomoda = metodo.criarString(numQtd);
+      Console.WriteLine($"{incomoda}muito mais");
+   }
+}
