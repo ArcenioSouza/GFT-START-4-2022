dec841d7b7473ea2637e9a5de3972d80b29514e4
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-24 16:25:56 -0300

    Adiciona teste 1

---
+++ testeSenhaValida/Program.cs
@@ -0,0 +1,29 @@
+﻿public class SenhaValida
+{  
+   int senhaValida = 2018;
+   public bool verificarSenha(int senha)
+   {  
+      if(senha == senhaValida){
+         return true;
+      } else {
+         return false;
+      }
+   }
+   public static void Main(string[] args)
+   {
+      bool validacaoSenha = false;
+      SenhaValida metodo = new SenhaValida();
+
+      while (validacaoSenha == false)
+      {
+         Console.WriteLine("Digite a sua senha de 4 digitos");
+         string strSenha = Console.ReadLine();
+         int intSenha = Int16.Parse(strSenha);
+         validacaoSenha = metodo.verificarSenha(intSenha);
+         if(validacaoSenha == false){
+            Console.WriteLine("Senha inválida");
+         }
+      }
+      Console.WriteLine("Acesso Garantido");
+   }
+}
